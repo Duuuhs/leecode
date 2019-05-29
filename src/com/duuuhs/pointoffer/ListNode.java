59282bef74c6f06a97ce0627f862a74c241b6412
Duuuhs <1948643024@qq.com>
2019-05-29 10:37:44 +0800

    链表结构

--- src/com/duuuhs/pointoffer/ListNode.java
+++ src/com/duuuhs/pointoffer/ListNode.java
@@ -3,7 +3,7 @@ package com.duuuhs.pointoffer;
 /**
  * @Author: DMY
  * @Date: 2019/4/8 16:03
- * @Description:  自定义一个链表结构,包括值和指向下一个节点的指针。
+ * @Description:  自定义一个链表结构,该结构包括值和指向下一个节点的指针。
  */
 public class ListNode {
     Object val;
