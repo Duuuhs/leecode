b58361fa98e3eda4718ec85b2aca120d0af9e0b3
Duuuhs <1948643024@qq.com>
2019-05-29 10:32:10 +0800

    剑指offer第三题

---
+++ src/com/duuuhs/pointoffer/ListNode.java
@@ -0,0 +1,17 @@
+package com.duuuhs.pointoffer;
+
+/**
+ * @Author: DMY
+ * @Date: 2019/4/8 16:03
+ * @Description: 自定义一个链表结构
+ */
+public class ListNode {
+    Object val;
+    ListNode next = null;
+
+    public ListNode(Object val){
+        this.val = val;
+    }
+
+
+}
---
+++ src/com/duuuhs/pointoffer/Solution3.java
@@ -0,0 +1,38 @@
+package com.duuuhs.pointoffer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @Author: DMY
+ * @Date: 2019/4/8 15:41
+ * @Description: (编程题) 输入一个链表，按链表值从尾到头的顺序返回一个ArrayList。
+ */
+public class Solution3 {
+
+    public static List printListFromTailToHead(ListNode listNode){
+        Stack<Object> stack = new Stack<>();
+        while (listNode != null ){
+            stack.push(listNode.val);
+            listNode = listNode.next;
+
+        }
+        List<Object> list = new ArrayList<>();
+        while (!stack.isEmpty()){
+            list.add(stack.pop());
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode("1");
+        listNode1.next = new ListNode("2");
+        listNode1.next.next = new ListNode("3");
+        listNode1.next.next.next = new ListNode("4");
+        listNode1.next.next.next.next = new ListNode("5");
+
+        List list = printListFromTailToHead(listNode1);
+        System.out.println(list);
+    }
+}
