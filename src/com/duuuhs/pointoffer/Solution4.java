379e94661ab5127db1f9c78b2c23e3bff75b0549
Duuuhs <1948643024@qq.com>
2019-06-03 21:58:51 +0800

    剑指offer第四题(重建二叉树)

--- src/com/duuuhs/pointoffer/Solution4.java
+++ src/com/duuuhs/pointoffer/Solution4.java
@@ -10,21 +10,25 @@ package com.duuuhs.pointoffer;
  *              则重建二叉树并返回。
  */
 public class Solution4 {
+    //一开始调用这个方法,pre代表前序遍历,in代表中序遍历
     public static TreeNode reConstructBinaryTree(int[] pre, int[] in){
         TreeNode root = reConstructBinaryTree(pre, 0, pre.length - 1, in, 0, in.length - 1);
         return root;
     }
 
-
+    //循环递归这个方法
     public static TreeNode reConstructBinaryTree(int[] pre, int startPre, int endPre, int[] in, int startIn, int endIn){
         if (startPre > endPre || startIn > endIn){
             return null;
         }
+        //前序遍历的startPre上的数值作为树的根
         TreeNode root = new TreeNode(pre[startPre]);
         for (int i = startIn; i <= endIn; i++){
             if (in[i] == pre[startPre]){
-                root.left = reConstructBinaryTree(pre, startPre+1, startPre+i-startIn, in, startIn, i-1);
+                //endPre参数的意思: i-startIn表示的是左子树的长度，再加上startPre就表示左子树结束的位置下标
+                root.left = reConstructBinaryTree(pre, startPre+1, i-startIn+startPre, in, startIn, i-1);
                 root.right = reConstructBinaryTree(pre, i-startIn+startPre+1, endPre, in, i+1, endIn);
+                //结束循环,减少匹配次数
                 break;
             }
         }
@@ -36,5 +40,6 @@ public class Solution4 {
         int[] in = {4,7,2,1,5,3,8,6};
         TreeNode treeNode = reConstructBinaryTree(pre, in);
         System.out.println(treeNode);
+
     }
 }
