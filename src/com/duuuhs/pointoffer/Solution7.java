42b25658b93aafe5a2167da5d5a8619d2e3bd5f1
Duuuhs <1948643024@qq.com>
2019-06-03 23:37:11 +0800

    剑指offer第七题(斐波那契数列第n项)

---
+++ src/com/duuuhs/pointoffer/Solution7.java
@@ -0,0 +1,45 @@
+package com.duuuhs.pointoffer;
+
+/**
+ * @Author: DMY
+ * @Date: 2019/6/3 23:08
+ * @Description: 输出斐波那契数列第n项
+ */
+public class Solution7 {
+
+    //方法1:递归,递归情况下有些n会出现stack溢出情况
+    public static int Fibonacci1(int n){
+        if (n == 0){
+            return 0;
+        } else if (n == 1){
+            return 1;
+        } else {
+            return Fibonacci1(n-1) + Fibonacci1(n-2);
+        }
+    }
+
+    //方法2:循环
+    public static int Fibonacci2(int n){
+        if (n == 0){
+            return 0;
+        }
+        if (n == 1){
+            return 1;
+        }
+        int prePreNum = 0;
+        int preNum = 1;
+        int result = 0;
+        for (int i = 2; i <= n; i++){
+            result = preNum + prePreNum;
+            prePreNum = preNum;
+            preNum = result;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int n = 20;
+        System.out.println(Fibonacci1(n));
+        System.out.println(Fibonacci2(n));
+    }
+}
