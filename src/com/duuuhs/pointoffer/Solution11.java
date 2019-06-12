e00571b7d7b50148e29e8de7dec1097e00a89ff5
Duuuhs <1948643024@qq.com>
2019-06-12 22:44:19 +0800

    剑指offer第十一题(二进制中1的个数)

--- src/com/duuuhs/pointoffer/Solution11.java
+++ src/com/duuuhs/pointoffer/Solution11.java
@@ -25,15 +25,6 @@ public class Solution11 {
         return count;
     }
 
-    public static int Number1(int n){
-        int count = 0;
-        while (n != 0){
-            ++count;
-            n = n & (n-1);
-        }
-        return count;
-    }
-
 
     public static void main(String[] args) {
         int n = 1011;
